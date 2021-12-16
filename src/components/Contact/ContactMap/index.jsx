1f04750f8a85293a77875499a4e25e5d6e0014ac
KeerhemFR <keerhem.guillaume@hotmail.fr>
2021-12-16 16:15:16 +0100

    Import components style

--- src/components/Contact/ContactMap/index.jsx
+++ src/components/Contact/ContactMap/index.jsx
@@ -1,19 +1,28 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import SContactMap from './SContactMap';
 
 export default function ContactMap() {
   return (
-    <div className="testMap">
-      <MapContainer center={[48.8786204, 2.3779959]} zoom={13} scrollWheelZoom>
+    <SContactMap>
+      <MapContainer center={[48.8786204, 2.3779959]} zoom={15} scrollWheelZoom>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[48.8786204, 2.3779959]}>
           <Popup>
-            Cabinet du Dr.Jeannerot <br /> 29 rue Manin, 75019 Paris
+            Hôpital Fondation A. de Rothschild - Département
+            d&apos;ophtalmologie
+            <br /> 29 rue Manin, 75019 Paris
+          </Popup>
+        </Marker>
+        <Marker position={[48.8784941, 2.3757991]}>
+          <Popup>
+            Institut Laser Vision Noémie de Rothschild
+            <br /> 44 Avenue Mathurin Moreau, 75019 Paris
           </Popup>
         </Marker>
       </MapContainer>
-    </div>
+    </SContactMap>
   );
 }
