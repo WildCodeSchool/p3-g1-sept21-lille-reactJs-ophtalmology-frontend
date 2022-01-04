2259ccee7ef82cea4928a586581b46b382430c1b
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2022-01-04 17:48:35 +0100

    mobile version of contact

--- src/components/Contact/ContactForm/style.jsx
+++ src/components/Contact/ContactForm/style.jsx
@@ -16,25 +16,42 @@ const SFormContact = styled.form`
     flex-direction: column;
     input {
       margin: 5px;
-      height: 2vw;
+      height: 4vh;
       border-radius: 5px;
       border: 2px solid var(--mColor);
     }
     textarea {
       margin: 5px;
-      height: 3vw;
+      height: 12vh;
       border-radius: 5px;
       border: 2px solid var(--mColor);
     }
   }
   .button {
-    width: 10vw;
-    height: 2vw;
+    width: 30%;
+    height: 7vh;
     align-self: center;
     border: 2px solid var(--mColor);
     border-radius: 5px;
     background-color: var(--mColor);
     color: white;
+    font-size: large;
   }
+
+    @media only screen and (max-width: 1100px) {
+      width: 80%;
+ 
+      }
+      .button {
+        width :30%;
+
+      }
+  
+  @media only screen and (max-width: 600px) {
+    width: 80%;
+    .button {
+        width :45%;
+
+      }
 `;
 export default SFormContact;
--- src/components/Contact/ContactInfo/style.jsx
+++ src/components/Contact/ContactInfo/style.jsx
@@ -12,8 +12,8 @@ const SContactDiv = styled.div`
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
-    width: 20vw;
-    min-width: 10%;
+    width: 23vw;
+
     padding: 2em;
     margin-top: 2em;
     box-shadow: inset -1px 3px 8px 5px #1f87ff, 2px 5px 16px 0px #0b325e,
@@ -46,13 +46,20 @@ const SContactDiv = styled.div`
     section {
       width:30%
     }
-    @media only screen and (max-width: 700px) {
+    width {
+      width: 10vh;
+    }
+    @media only screen and (max-width: 600px) {
       flex-direction: column;
       align-items: center;
       
     section {
-      width: 80%
+      width: 80%;
+      height : auto;
     }  
+    img {
+      width: 10vw
+    }
 `;
 
 export default SContactDiv;
