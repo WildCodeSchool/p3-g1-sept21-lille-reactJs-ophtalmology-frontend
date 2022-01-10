24e7ed57596ee5edd6d7044159c0e7236d07e2ca
Romain Brehon <romain.brehon@yahoo.fr>
2022-01-10 17:26:22 +0100

    made requested changes

--- src/components/Cataract/index.jsx
+++ src/components/Cataract/index.jsx
@@ -1,20 +1,17 @@
 import cataractContent from './content';
-import SCataract from './style';
 import DropdownWindow from '../DropdownWindows/index';
 
 function Cataract() {
   return (
-    <SCataract>
-      <>
-        {cataractContent.map((cataract) => (
-          <DropdownWindow
-            key={cataract.id}
-            title={cataract.title}
-            content={cataract.content}
-          />
-        ))}
-      </>
-    </SCataract>
+    <>
+      {cataractContent.map((cataract) => (
+        <DropdownWindow
+          key={cataract.id}
+          title={cataract.title}
+          content={cataract.content}
+        />
+      ))}
+    </>
   );
 }
 
--- src/components/Cataract/style.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const SCataract = styled.section`
-  @media only screen and (min-width: 700px) {
-  }
-`;
-
-export default SCataract;
