b4795efd34862a1ed35558c3d7850b45e6e14eff
Romain Brehon <romain.brehon@yahoo.fr>
2022-01-13 09:50:50 +0100

    fixed the dropdownWindows css. was trying to include transition to elements that move with state, but gave up so there might be slight changes in the Intro/index

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Footer from './components/Footer';
 import Cataract from './components/Cataract';
+
 import './reset.css';
 import './App.css';
 
