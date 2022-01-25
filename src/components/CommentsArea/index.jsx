140415991ab48a7f590f1a8bb65be57c967b7976
Romain Brehon <romain.brehon@yahoo.fr>
2022-01-25 11:14:26 +0100

    We can now post a comment into db and were able to read comments up until the point where we tried to load comments on a per page basis

--- src/components/CommentsArea/index.jsx
+++ src/components/CommentsArea/index.jsx
@@ -1,124 +1,143 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import doctorAvatar from '../../assets/doctorAvatar.png';
+// import doctorAvatar from '../../assets/doctorAvatar.png';
 import userAvatar from '../../assets/userAvatar.png';
 import SComments from './commentsStyle';
 import SCommentsArea from './style';
 
-/* const dicoPages = {
-  videos: 1,
-  glaucoma: 2,
-}; */
-
 function CommentsArea() {
-  const [form] = useState({
-    authorName: 'Romain',
-    message: 'Bla',
-    date: '2022/01/19',
-    idPage: useLocation().pathname,
-  });
+  // GET COMMENTS FROM DB
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:5050/comments')
+      .then((res) => res.data)
+      .then((data) => {
+        setCommentList(data);
+      })
+      .catch((err) => {
+        setCommentList('Cette page ne contient pas encore de commentaires.');
+        console.log(err);
+      });
+  }, []);
 
-  // const [nameChange, setNameChange] = useState('');
-  // const onNameChange = (evt) => setNameChange({ authorName: evt.target.value });
+  // INPUT HANDLERS
+  const [nameChange, setNameChange] = useState('');
+  const onNameChange = (evt) => setNameChange(evt.target.value);
 
   const [messageChange, setMessageChange] = useState('');
-  const onMessageChange = (evt) =>
-    setMessageChange({ message: evt.target.value });
+  const onMessageChange = (evt) => setMessageChange(evt.target.value);
 
+  // FORM ?
+  const dicoPages = {
+    '/': 1,
+    '/glaucoma': 2,
+    '/cataract': 3,
+    '/refractive': 4,
+  };
+  const pageUrl = useLocation().pathname;
+
+  const [form, setForm] = useState({
+    authorName: nameChange,
+    message: messageChange,
+    idPages: dicoPages[pageUrl],
+  });
+  useEffect(() => {
+    setForm({
+      ...form,
+      authorName: nameChange,
+      message: messageChange,
+    });
+  }, [nameChange, messageChange]);
+
+  useEffect(() => {
+    setForm({
+      ...form,
+      idPages: dicoPages[pageUrl],
+    });
+  }, [pageUrl]);
+
+  // POST A COMMENT
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const commentData = {
       authorName: form.authorName,
       message: form.message,
-      date: form.date,
       idPages: form.idPages,
-      idParent: form.idParent,
     };
     axios
-      .post('http:localhost:5050/comments', commentData)
-      .then((res) => console.log(res))
+      .post('http://localhost:5050/comments', commentData)
+      .then(() =>
+        setCommentList([
+          ...commentList,
+          {
+            id: 0,
+            message: form.message,
+            authorName: form.authorName,
+            date: Date.now(),
+          },
+        ])
+      )
       .catch((err) => console.log(err));
   };
 
-  const [commentList] = useState([]);
-
-  /*  const getComments = () => {
-    axios
-      .get('http:localhost:5050/comments')
-      .then((res) => res.data)
-      .then((data) => {
-        setCommentList(data.results);
-      });
-  }; */
-
   return (
     <SCommentsArea>
-      <form className="commentSection">
+      <form className="commentSection" onSubmit={handleSubmit}>
         <h3>Mes échanges avec les internautes</h3>
 
         <SComments>
-          {/* TEST WITH MAP 
-          WHERE DO I PUT THE 'getComments'? 
-          HOW DO I RETRIEVE THE 'idPages' and 'idParent'? */}
-          {commentList.map((comments) => (
-            <ul
-              key={comments.id}
-              authorName={comments.authorName}
-              message={comments.message}
-              date={comments.date}
-              idPages={comments.idPages}
-              idParent={comments.idParent}
-            >
-              <li className="comment userComment">
-                <div className="avatarContainer">
-                  <img className="avatars" src={userAvatar} alt="User Avatar" />
-                </div>
-                <div className="infoContainer">
-                  <p className="nameDisplay">{comments.authorName}</p>
-                  <div className="dateContainer">
-                    <p className="dateDisplay">Publié le {comments.date}</p>
+          {/* TEST WITH MAP */}
+          <ul>
+            {commentList
+              .filter((data) => {
+                return parseInt(data.idPages, 10) === dicoPages[pageUrl];
+              })
+              .map((comments) => (
+                <li
+                  key={comments.id}
+                  authorName={comments.authorName}
+                  message={comments.message}
+                  date={comments.date}
+                  idPages={comments.idPages}
+                  className="comment userComment"
+                >
+                  <div className="avatarContainer">
+                    <img
+                      className="avatars"
+                      src={userAvatar}
+                      alt="User Avatar"
+                    />
                   </div>
-                  <p className="messageDisplay">{comments.message}</p>
-                </div>
-              </li>
+                  <div className="infoContainer">
+                    <p className="nameDisplay">{comments.authorName}</p>
+                    <div className="dateContainer">
+                      <p className="dateDisplay">Publié le {comments.date}</p>
+                    </div>
+                    <p className="messageDisplay">{comments.message}</p>
+                  </div>
+                </li>
+              ))}
 
-              {/* IF COMMENT IS NOT BY AL-J */}
-              <li className="comment userComment">
-                <div className="avatarContainer">
-                  <img className="avatars" src={userAvatar} alt="User Avatar" />
+            {/* IF COMMENT IS BY AL-J */}
+            {/* <li className="comment answerComment">
+              <div className="avatarContainer">
+                <img
+                  className="avatars"
+                  src={doctorAvatar}
+                  alt="Doctor Avatar"
+                />
+              </div>
+              <div className="infoContainer">
+                <p className="nameDisplay">{comments.authorName}</p>
+                <div className="dateContainer">
+                  <p className="dateDisplay">Publié le {comments.date}</p>
                 </div>
-                <div className="infoContainer">
-                  <p className="nameDisplay">{comments.authorName}</p>
-                  <div className="dateContainer">
-                    <p className="dateDisplay">Publié le {comments.date}</p>
-                    <button className="replyBtn" type="submit">
-                      RÉPONDRE
-                    </button>
-                  </div>
-                  <p className="messageDisplay">{comments.message}</p>
-                </div>
-              </li>
-
-              {/* IF COMMENT IS BY AL-J */}
-              <li className="comment answerComment">
-                <div className="avatarContainer">
-                  <img
-                    className="avatars"
-                    src={doctorAvatar}
-                    alt="Doctor Avatar"
-                  />
-                </div>
-                <div className="infoContainer">
-                  <p className="nameDisplay">{comments.authorName}</p>
-                  <div className="dateContainer">
-                    <p className="dateDisplay">Publié le {comments.date}</p>
-                  </div>
-                  <p className="messageDisplay">{comments.message}</p>
-                </div>
-              </li>
-            </ul>
-          ))}
+                <p className="messageDisplay">{comments.message}</p>
+              </div>
+            </li> */}
+          </ul>
         </SComments>
         <h5>Laissez votre commentaire</h5>
         <div className="writeComment">
@@ -126,9 +145,9 @@ function CommentsArea() {
           <input
             className="messageInput"
             type="text"
-            placeholder="Votre nom ?"
-            value={form.authorName}
-            // onChange={onNameChange}
+            placeholder="Votre nom"
+            value={nameChange}
+            onChange={onNameChange}
           />
           <p className="inputTitle">Message</p>
           <input
@@ -139,9 +158,7 @@ function CommentsArea() {
             onChange={onMessageChange}
           />
           <div className="btnDiv">
-            <button className="sendBtn" type="submit" onSubmit={handleSubmit}>
-              ENVOYER
-            </button>
+            <input className="sendBtn" type="submit" value="ENVOYER" />
           </div>
         </div>
       </form>
