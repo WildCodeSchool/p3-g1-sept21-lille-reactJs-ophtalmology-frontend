9e9bfc760830c9588e749aea8569077d3c20be38
BenZen59 <benoit.zenone@gmail.com>
2022-01-26 16:12:01 +0100

    Add route for the different section and create Link

--- src/components/Refractive/index.jsx
+++ src/components/Refractive/index.jsx
@@ -1,16 +1,34 @@
+import { PropTypes } from 'prop-types';
 import DropdownWindow from 'components/DropdownWindows';
 import refractiveContent from './content';
 
-export default function Refractive() {
+export default function Refractive({ state }) {
   return (
     <>
-      {refractiveContent.map((refractive) => (
-        <DropdownWindow
-          key={refractive.id}
-          title={refractive.title}
-          content={refractive.content}
-        />
-      ))}
+      {refractiveContent
+        .filter((refractive) => refractive.id === state)
+        .map((refractive) => (
+          <DropdownWindow
+            key={refractive.id}
+            title={refractive.title}
+            content={refractive.content}
+            Dropdown={false}
+          />
+        ))}
+      {refractiveContent
+        .filter((refractive) => refractive.id < state || refractive.id > state)
+        .map((refractive) => (
+          <DropdownWindow
+            key={refractive.id}
+            title={refractive.title}
+            content={refractive.content}
+            Dropdown
+          />
+        ))}
     </>
   );
 }
+
+Refractive.propTypes = {
+  state: PropTypes.number.isRequired,
+};
