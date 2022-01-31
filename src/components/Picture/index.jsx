5a11b013fb38e72c9fe28be679e031336ef8f4af
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-01-31 14:45:08 +0100

    Add some logical to check if the image is an URL or an image in the public folder

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import SPicture from './style';
 
 export default function Pictures({ imgUrl, imgDesc }) {
+  const [urlCheck] = useState(imgUrl.indexOf('http'));
   return (
     <SPicture>
       <img
-        src={`${process.env.REACT_APP_API_URL}/${imgUrl}` || null}
+        src={
+          urlCheck !== -1
+            ? imgUrl || null
+            : `${process.env.REACT_APP_API_URL}/${imgUrl}` || null
+        }
         alt={imgDesc || null}
       />
       <p>{imgDesc || null}</p>
--- src/components/Picture/style.jsx
+++ src/components/Picture/style.jsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 const SPicture = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
   img {
     width: 200px;
   }
   p {
     text-align: center;
+    width: 300px;
   }
 `;
 
